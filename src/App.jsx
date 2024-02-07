f7165f68eb8b6fa99d9f64fb26033dbc03dc8cc7
sevro49 <21120205303@ismu.edu.tr>
2024-02-07 14:57:56 +0300

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,58 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 
-function NameList() {
-  const [list, setList] = useState(["Jack", "Jill", "John"]);
-  const [name, setName] = useState("");
-
-  const onAddName = () => {
-    setList([...list, name]);
-    setName("")
-  }
+function UserForm() {
+  const [state, dispatch] = useReducer(
+    (state, action) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      first: "",
+      last: ""
+    });
 
   return (
     <div>
-      <ul>
-        {list.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)} />
-        <button
-        onClick={onAddName}>
-          Add Name
-        </button>
+      <input type="text" value={state.first} onChange={(e) => dispatch({ first: e.target.value })} />
+      <input type="text" value={state.last} onChange={(e) => dispatch({ last: e.target.value })} />
+      <div>
+        First: {state.first}
+      </div>
+      <div>
+        Last: {state.last}
+      </div>
     </div>
   )
 }
+function NameList() {
 
-function Counter() {
-  let [count, setCount] = useState(10);
-
-  function addOne() {
-    setCount(count + 1);
-  }
-
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "SET_NAME":
+        return { ...state, name: action.payload };
+      case "ADD_NAME":
+        return {
+          ...state, names: [...state.names, state.name],
+          name: ""
+        };
+    }
+  }, {
+    names: [],
+    name: "",
+  })
   return (
-    <div className='App'>
-      <button onClick={addOne}>Count = {count} </button>
+    <div>
+      <div>
+        {state.names.map((name, index) => (
+          <div key={index}>{name}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={state.name}
+        onChange={e => dispatch({ type: "SET_NAME", payload: e.target.value })} />
+
+      <button onClick={() => dispatch({ type: "ADD_NAME" })}>Add Name</button>
     </div>
   )
 }
@@ -45,9 +60,10 @@ function Counter() {
 function App() {
   return (
     <div className='App'>
-      <Counter />
+      <UserForm />
       <NameList />
     </div>
   )
 }
+
 export default App
